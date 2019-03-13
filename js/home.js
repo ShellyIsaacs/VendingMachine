741955eeac0b8405d40deddd1de45f51cce4379f
ShellyIsaacs <45702961+ShellyIsaacs@users.noreply.github.com>
2019-03-13 09:12:55 -0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
     loadItems();
 })
 
+function getCardColor(){
+  var colors = [ "#67CDD3", "#FEB24F", "#E5493C", "#CFBEB0", "#9ECF8B" ];
+  var random = Math.floor(Math.random() * 5);
+  return colors[random];
+}
+
 function loadItems(){
   clearItems();
   var vendingItems = $('#vendingItems');
@@ -15,11 +21,11 @@ function loadItems(){
         $.each(data, function(index, item) {
 
           var card = '<div class = "col-md-4 col-sm-6">';
-          card += '<div class = "card btn text-white bg-dark" style = "margin: 10px" onclick = "enterItem('+ item.id +')">';
-          card += '<p style = "text-align: left; padding: 0; margin: 0">'+item.id+'</p>';
-          card += '<p>'+item.name+'</p>';
-          card += '<p>$'+item.price+'</p>';
+          card += '<div class = "card btn" style = "margin: 10px; background-color: '+getCardColor()+'" onclick = "enterItem('+ item.id +')">';
+          card += '<p style = "text-align: left; padding: 0 0 5px 0; margin: 0">'+item.id+'</p>';
+          card += '<p id = "itemName">'+item.name+'</p>';
           card += '<br>';
+          card += '<p>$'+item.price+'</p>';
           card += '<p>Quantity Left: '+item.quantity+'</p>';
           card += '</div></div>';
           vendingItems.append(card);
@@ -46,7 +52,7 @@ function enterItem(itemId) {
 }
 
 $("#returnChangeButton").click(function() {
-  $('#message').html('&emsp;&emsp;&emsp;&emsp;');
+  $('#message').html('-');
   var quarters = Math.floor(money/25);
   var remainder = money%25;
   var dimes = Math.floor(remainder/10);
@@ -54,8 +60,8 @@ $("#returnChangeButton").click(function() {
   var nickels = Math.floor(remainder/5);
   var remainder = remainder%5;
   var pennies = remainder;
-  $('#change').text(quarters + ' Quarters ' + dimes + ' Dimes ' + nickels + ' Nickels ' + pennies + ' Pennies');
-  $('#moneyIn').html('&emsp;&emsp;&emsp;&emsp;');
+  $('#change').text(quarters + ' Quarters, ' + dimes + ' Dimes, ' + nickels + ' Nickels, ' + pennies + ' Pennies');
+  $('#moneyIn').html('0');
   money = 0;
 })
 
@@ -75,8 +81,8 @@ $('#purchaseButton').click(function() {
       success: function(data, status) {
         $('#message').text('Thank You!!!');
         money = 0;
-        $('#change').text(data.quarters + ' Quarters ' + data.dimes + ' Dimes ' + data.nickels + ' Nickels ' + data.pennies + ' Pennies');
-        $('#moneyIn').html('&emsp;&emsp;&emsp;&emsp;');
+        $('#change').text(data.quarters + ' Quarters, ' + data.dimes + ' Dimes, ' + data.nickels + ' Nickels, ' + data.pennies + ' Pennies');
+        $('#moneyIn').html('0');
         loadItems();
       },
       error: function (xhr, status, error) {
